b64f1c1579851947c319a54dbdb0c9a1acf4fc41
Justin Simon <justinsimon@Justins-MacBook-Pro.local>
2024-01-14 14:58:46 -0800

    Creating project

---
+++ LogProject_1.14.24/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LogProject_1._14._24.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
